82c13b3f68c1a69490fa35218e216a87b5aea31f
zyad37-3 <zyad.7moda27@gmail.com>
2026-04-11 20:13:59 +0200

    update project

--- src/utils/getmytoken.ts
+++ src/utils/getmytoken.ts
@@ -7,7 +7,9 @@ import { cookies } from "next/headers";
 
 export default async function getmytoken() {
  const mycookies= await cookies()
- const tokenfromcookies=mycookies.get("next-auth.session-token")?.value
+ const tokenfromcookies=
+    mycookies.get("next-auth.session-token")?.value ||
+    mycookies.get("__Secure-next-auth.session-token")?.value;
 console.log("my token",tokenfromcookies);
 
 const mytokenafterDecoded=await decode({token:tokenfromcookies,secret:process.env.NEXTAUTH_SECRET!})
